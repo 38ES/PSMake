77937d128adb718172626c78fd4abe7b871901e6
Keith Jackson <keith.jackson.11@us.af.mil>
2022-01-27 08:27:08 -0600

    Changed import of system library to psm1 instead of manifest

--- make.psm1
+++ make.psm1
@@ -1,2 +1,10 @@
+# Import System.Security from GAC if Windows PowerShell is being used
+if ( ([version]$PSVersionTable.PSVersion).Major -lt 6) {
+    $asm = [System.Reflection.Assembly]::LoadFromPartialName("System.Seurity")
+    if ($null -eq $asm) {
+        throw 'Unable to load System.Security from GAC for Windows PowerShell'
+    }
+}
+
 # Include all the functions in the functions folder
 Get-ChildItem "$PSScriptRoot\functions\" -Recurse -File | ForEach-Object { . $_.FullName }
